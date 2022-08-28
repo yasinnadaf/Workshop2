1e1a6fab0f6532f3baf75f009a5c13a7e558bbcc
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-28 22:46:16 +0530

    workshop problem solved

--- src/com/bridgelabz/workshop2/AddressBook.java
+++ src/com/bridgelabz/workshop2/AddressBook.java
@@ -25,4 +25,40 @@ public class AddressBook {
 
     }
 
+    void editContact(){
+        System.out.println("Enter the name of person to edit");
+        String name = scanner.next();
+        for(Contacts contact : contactList){
+            if(name.equals(contact.getName())){
+                System.out.println("Edit the details of person: ");
+
+                System.out.println("edit name");
+                contact.setName(scanner.next());
+                System.out.println("Enter age: ");
+                contact.setAge(scanner.next());
+                System.out.println("Enter city: ");
+                contact.setCity(scanner.next());
+                System.out.println("Enter state: ");
+                contact.setState(scanner.next());
+                System.out.println("Enter Phone number");
+                contact.setPhoneNumber(scanner.nextLong());
+            }
+        }
+    }
+    void searchName(){
+        System.out.println("enter the name of person you want to search: ");
+        String name = scanner.next();
+        for(Contacts search : contactList){
+            if(name.equals(search.getName())){
+                System.out.println(search);
+            }
+        }
+    }
+
+    void displayContacts(ArrayList<Contacts> contactList){
+        for(Contacts contact : this.contactList){
+            System.out.println(contact.toString());
+        }
+    }
+
 }
--- src/com/bridgelabz/workshop2/AddressBookMain.java
+++ src/com/bridgelabz/workshop2/AddressBookMain.java
@@ -1,20 +1,87 @@
 package com.bridgelabz.workshop2;
 
 import java.util.HashMap;
+import java.util.*;
 
 public class AddressBookMain {
-    static final String MY_ADDRESSBOOK = "my address book";
+    static String currentAddressBook;
+    static String addressBookName;
+    static HashMap<String, AddressBook> addressBookMap = new HashMap<>();
+    static Scanner scr = new Scanner(System.in);
 
     public static void main(String[] args) {
+        boolean exit = false;
 
-        HashMap<String, AddressBook> addressBookMap = new HashMap<>();
-        AddressBook myAddressBook = new AddressBook();
+        while (!exit){
+            System.out.println("Enter options:\n1) To add contact\n2) To edit Contact\n3) To search name\n4) To add address book or select addressBook\n5) To exit");
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    try {
+                        addressBookMap.get(currentAddressBook).addContact();
+                    } catch (Exception e){
+                        System.out.println("\nNo AddressBook Found\n");
 
-        myAddressBook.addContact();
-        System.out.println(myAddressBook.contactList);
-
-        addressBookMap.put(MY_ADDRESSBOOK,myAddressBook);
+                    }
+                    break;
+                case 2:
+                    try {
+                        addressBookMap.get(currentAddressBook).editContact();
+                    } catch (Exception e){
+                        System.out.println("\nNo AddressBook Found\n");
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    try {
+                        addressBookMap.get(currentAddressBook).searchName();
+                    } catch (Exception e){
+                        System.out.println("\nNo AddressBook Found\n");
+                    }
+                    break;
+                case 4:
+                    chooseAddressBook();
+                    break;
+                case 5:
+                    exit = true;
+                default:
+                    break;
+            }
+        } System.out.println(addressBookMap);
 
     }
+        static void chooseAddressBook () {
+            System.out.println("""
+                    Press 1 to add AddressBook
+                    Press 2 to select AddressBook""");
 
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter address book name");
+                    addressBookName = scr.next().toLowerCase();
+                    if (addressBookMap.containsKey(addressBookName)) {
+                        System.out.println("\nAddress book already exist\n");
+                        chooseAddressBook();
+                    } else {
+                        AddressBook addressBook = new AddressBook();
+                        addressBookMap.put(addressBookName, addressBook);
+                        currentAddressBook = addressBookName;
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Enter address book name");
+                    addressBookName = scr.next().toLowerCase();
+                    if (!addressBookMap.containsKey(addressBookName)) {
+                        System.out.println("\nAddressBook not Found\n");
+                        chooseAddressBook();
+                    } else
+                        currentAddressBook = addressBookName;
+                    break;
+                default:
+                    break;
+            }
+
+        }
 }
